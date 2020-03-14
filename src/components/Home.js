3659bcfeeca592ee00073aa4bd56aea383ad3727
raulbarboza <ruzito@gmail.com>
2020-03-14 17:11:00 -0300

    fixed art.png

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends React.Component {
       <div className="container">
         <Nav />
         <HomeContent />
-        <img className="img-home" src='https://firebasestorage.googleapis.com/v0/b/observatorio-animal.appspot.com/o/art.png?alt=media&token=7c3e8f56-5265-41d1-bb58-ddbba6c7ae37' />
+        <img className="img-home" src='https://firebasestorage.googleapis.com/v0/b/observatorio-animal.appspot.com/o/art.png?alt=media&token=035f92fc-ff48-4341-a4d6-7d0b2aff5d03' />
       </div>
     )
   }
